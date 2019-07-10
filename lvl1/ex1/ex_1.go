6d99f41bc59d8dcc4dbaadf87c69fb5067959376
Alexander Babai <aliaksandr.babai@gmail.com>
2019-07-10 15:00:15 +0300

    refactor(lvl1.ex1): update docs

--- lvl1/ex1/ex_1.go
+++ lvl1/ex1/ex_1.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// Resolve is a function that provide solution of exercise ninja-level-1
+// Resolve is a function that provide solution of exercise 2 ninja-level-1
 func Resolve() {
 	fmt.Println("Exercise 1")
 
@@ -13,8 +13,10 @@ func Resolve() {
 	z := true
 
 	fmt.Println(x, y, z)
+
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Println(z)
+	
 	fmt.Println()
 }
