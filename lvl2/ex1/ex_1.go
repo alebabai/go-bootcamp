59c02ef4438f8db2b0c615bfe1094b7e1f2a3052
Alexander Babai <aliaksandr.babai@gmail.com>
2019-07-30 16:23:54 +0300

    feat(lvl2): add ex1

---
+++ lvl2/ex1/ex_1.go
@@ -0,0 +1,15 @@
+package ex1
+
+import (
+	"fmt"
+)
+
+// Resolve is a function that provide solution of exercise 1 ninja-level-2
+func Resolve() {
+	const number = 100
+
+	fmt.Println("Exercise 1")
+	fmt.Printf("%d\t\t%b\t\t%#x", number, number, number)
+
+	fmt.Println()
+}
