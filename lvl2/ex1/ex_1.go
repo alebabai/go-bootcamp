559b090a697b91c0d1d46ee18a3a00d837c1d3bb
Alexander Babai <aliaksandr.babai@gmail.com>
2019-07-30 16:27:57 +0300

    refactor(lvl2.ex1): beautify

--- lvl2/ex1/ex_1.go
+++ lvl2/ex1/ex_1.go
@@ -6,9 +6,9 @@ import (
 
 // Resolve is a function that provide solution of exercise 1 ninja-level-2
 func Resolve() {
-	const number = 100
-
 	fmt.Println("Exercise 1")
+
+	number := 100
 	fmt.Printf("%d\t\t%b\t\t%#x", number, number, number)
 
 	fmt.Println()
