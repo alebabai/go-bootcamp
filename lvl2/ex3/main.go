181f2eaa36868c1c4ed98eb59703f5800a0395f0
Alexander Babai <aliaksandr.babai@gmail.com>
2019-07-30 16:37:07 +0300

    feat(lvl2): add ex3

---
+++ lvl2/ex3/main.go
@@ -0,0 +1,18 @@
+package ex3
+
+import (
+	"fmt"
+)
+
+// Resolve is a function that provide solution of exercise 3 ninja-level-2
+func Resolve() {
+	fmt.Println("Exercise 3")
+
+	const (
+		a     = 13
+		b int = 13
+	)
+	fmt.Println(a, b)
+
+	fmt.Println()
+}
