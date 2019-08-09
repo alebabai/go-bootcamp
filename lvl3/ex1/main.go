8af40a65b3c2191d2efc09b622f9556b13b4d001
Alexander Babai <aliaksandr.babai@gmail.com>
2019-08-09 16:24:40 +0300

    feat(lvl3): add ex1

---
+++ lvl3/ex1/main.go
@@ -0,0 +1,16 @@
+package ex1
+
+import (
+	"fmt"
+)
+
+// Resolve is a function that provide solution of exercise 1 ninja-level-3
+func Resolve() {
+	fmt.Println("Exercise 1")
+
+	for i := 1; i <= 100; i++ {
+		fmt.Printf("%d\t", i)
+	}
+
+	fmt.Println()
+}
---
+++ lvl3/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/alebabai/go-bootcamp/lvl3/ex1"
+)
+
+func main() {
+	ex1.Resolve()
+}
