8296da20c517d297dd8f8639c2f08ea92e4d02c9
Alexander Babai <aliaksandr.babai@gmail.com>
2019-08-29 15:59:04 +0300

    feat(lvl4): add ex1

---
+++ lvl4/ex1/main.go
@@ -0,0 +1,20 @@
+package ex1
+
+import (
+	"fmt"
+)
+
+// Resolve is a function that provide solution of exercise 1 ninja-level-4
+func Resolve() {
+	fmt.Println("Exercise 1")
+
+	arr := []int{1, 2, 3, 4, 13}
+
+	for i, v := range arr {
+		fmt.Printf("Index: %d\tvalue: %v\n", i, v)
+	}
+
+	fmt.Printf("Array's type: %T", arr)
+
+	fmt.Println()
+}
---
+++ lvl4/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/alebabai/go-bootcamp/lvl4/ex1"
+)
+
+func main() {
+	ex1.Resolve()
+}
