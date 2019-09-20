0c32114bf8384251779a6fa253ebcb14c011dc61
Alexander Babai <aliaksandr.babai@gmail.com>
2019-09-20 16:17:28 +0300

    feat(lvl7): add ex2

---
+++ lvl7/ex2/main.go
@@ -0,0 +1,27 @@
+package ex2
+
+import (
+	"fmt"
+)
+
+type person struct {
+	name string
+}
+
+func changeMe(p *person) {
+	p.name = "Taller Derden"
+}
+
+// Resolve is a function that provide solution of exercise 2 ninja-level-7
+func Resolve() {
+	fmt.Println("Exercise 2")
+
+	p1 := person{
+		name: "The storyteller",
+	}
+	fmt.Println(p1)
+	changeMe(&p1)
+	fmt.Println(p1)
+
+	fmt.Println()
+}
