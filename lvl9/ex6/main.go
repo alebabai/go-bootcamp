e887439f1ca0923ad486cfeb1dfe851838f9ba9a
Alexander Babai <aliaksandr.babai@gmail.com>
2019-09-23 17:05:06 +0300

    feat(lvl9): add ex6

---
+++ lvl9/ex6/main.go
@@ -0,0 +1,15 @@
+package ex6
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Resolve is a function that provide solution of exercise 6 ninja-level-9
+func Resolve() {
+	fmt.Println("Exercise 6")
+
+	fmt.Println(runtime.GOOS, runtime.GOARCH)
+
+	fmt.Println()
+}
