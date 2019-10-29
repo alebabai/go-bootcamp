17ed4341890eb52d4cafdb41b467b93ac5bc50eb
Alexander Babai <aliaksandr.babai@gmail.com>
2019-10-29 16:10:10 +0300

    feat(lvl12): add ex1

---
+++ lvl12/ex1/main.go
@@ -0,0 +1,16 @@
+package ex1
+
+import (
+	"fmt"
+
+	"github.com/alebabai/go-bootcamp/lvl12/ex1/dog"
+)
+
+// Resolve is a function that provide solution of exercise 1 ninja-level-12
+func Resolve() {
+	fmt.Println("Exercise 1")
+
+	fmt.Println(dog.Years(3))
+
+	fmt.Println()
+}
